8e507b7e5c0df0d15d4038b3a0ba7b9123b31590
Zhaolin Lau <zhaolinlau@gmail.com>
2025-03-06 00:07:51 +0800

    bump bootstrap dependencies and sass to latest version

--- src/Presets/Bootstrap.php
+++ src/Presets/Bootstrap.php
@@ -31,7 +31,7 @@ class Bootstrap extends Preset
         return [
             'bootstrap' => '^5.3.3',
             '@popperjs/core' => '^2.11.8',
-            'sass' => '^1.80.6',
+            'sass' => '^1.85.1',
         ] + $packages;
     }
 
